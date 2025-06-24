028f3cd5616d00475cfd262b14e41603ec17e6e6
Dani J <djordan020403@gmail.com>
2025-06-24 11:36:04 -0700

    feat: add get and update functions for daily_summary table (#16)

--- src/server/database/diary.js
+++ src/server/database/diary.js
@@ -40,8 +40,64 @@ async function deleteFood(uid, entry_id) {
     return result;
 }
 
+async function updateDailySummary(uid, item) {
+    let result;
+    const db = await connectDB();
+    const row = await db.get(`
+        SELECT calories, fat, carbs, protein 
+        FROM daily_summary
+        WHERE user_id=? AND date=?`,
+        [uid, item.date_eaten]
+    );
+
+    if (row) {
+        let updated_cal = Math.round((row.calories + item.cal) * 10) / 10;
+        let updated_fat = Math.round((row.fat + item.fat) * 10) / 10;
+        let updated_carb = Math.round((row.carbs + item.carb) * 10) / 10;
+        let updated_prot = Math.round((row.protein + item.prot) * 10) / 10;
+
+        result = await db.get(`
+            UPDATE daily_summary 
+            SET calories=?, fat=?, carbs=?, protein=?
+            WHERE user_id=? AND date=? 
+            RETURNING *`,
+            [updated_cal, updated_fat, updated_carb, updated_prot,
+            uid, item.date_eaten]
+        );
+    } else {
+        result = await db.get(`
+            INSERT INTO daily_summary 
+            (user_id, date, calories, fat, carbs, protein)
+            VALUES (?, ?, ?, ?, ?, ?)
+            RETURNING *`,
+            [uid, item.date_eaten, item.cal, item.fat, item.carb, item.prot]
+        );        
+    }
+    await db.close();
+    return result;
+}
+
+async function getWeeklySummary(uid, dates) {
+    let results = [];
+    const db = await connectDB();
+    for (const date of dates) {
+        let result = await db.get(`
+            SELECT calories, fat, carbs, protein 
+            FROM daily_summary
+            WHERE user_id=? AND date=?`,
+            [uid, date]
+        );
+        results.push(result);
+    }
+    await db.close()
+    console.log(results);
+    return results;
+}
+
 module.exports = {
     addFood,
     getFoodsByDate,
-    deleteFood
+    deleteFood,
+    updateDailySummary,
+    getWeeklySummary
 }
